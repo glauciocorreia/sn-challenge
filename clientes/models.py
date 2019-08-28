5505faf6b3044bed7b83c9318a0cf6ed12e8d6ee
glauciocorreia <glauciocorreiaa@gmail.com>
2019-08-27 23:57:07 -0300

    013 - Estilo de Clientes Excluir

--- clientes/models.py
+++ clientes/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 
 class Cliente(models.Model):
   nome = models.CharField(max_length=80)
-  telefone = PhoneNumberField(null=False, blank=False, unique=True)
+  telefone = models.CharField(max_length=15)
   endereco = models.CharField(max_length=100)
   numero = models.CharField(max_length=6)
   cidade = models.CharField(max_length=50)
   estado = models.CharField(max_length=100)
   pais = models.CharField(max_length=50)
-  cep = models.CharField(max_length=9)
+  cep = models.CharField(max_length=10)
   
 
   def __str__(self):
